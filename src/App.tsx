b04a7c4c456ae842a910ff01f9c7652a36ef98fa
fzsf163 <yofaisol619@gmail.com>
2024-09-12 04:00:14 +0600

    some updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import Navbar from "./components/navbar/navbar";
-import "./index.css";
-import LogoBox from "./components/logo-gorizontal/logo-box";
-import Transportation from "./components/tranportation/Transportation";
-import NumberCard from "./components/number-card/numberCard";
-import Services from "./components/services/services";
 import placholder from "./assets/heroimg/tom-barrett-M0AWNxnLaMw-unsplash.webp";
-import SafeJourney from "./components/safe-journey/SafeJourney";
-import Process from "./components/Process/Process";
 import GetInTouchForm from "./components/GetInTouchForm/GetInTouchForm";
+import Process from "./components/Process/Process";
 import Faq from "./components/faq/Faq";
 import Footer from "./components/footer/Footer";
+import LogoBox from "./components/logo-gorizontal/logo-box";
+import Navbar from "./components/navbar/navbar";
+import NumberCard from "./components/number-card/numberCard";
+import SafeJourney from "./components/safe-journey/SafeJourney";
+import Services from "./components/services/services";
+import Transportation from "./components/tranportation/Transportation";
+import "./index.css";
 function App() {
   return (
     <div className="w-full min-w-full">
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,108 +2,108 @@ import { useRef, useState } from "react";
 import "./navbar.css";
 
 const menuItems = [
-	{
-		label: "service",
-		id: 1,
-	},
-	{
-		label: "company",
-		id: 2,
-	},
-	{
-		label: "features",
-		id: 3,
-	},
-	{
-		label: "news",
-		id: 4,
-	},
-	{
-		label: "contact",
-		id: 5,
-	},
+  {
+    label: "service",
+    id: 1,
+  },
+  {
+    label: "company",
+    id: 2,
+  },
+  {
+    label: "features",
+    id: 3,
+  },
+  {
+    label: "news",
+    id: 4,
+  },
+  {
+    label: "contact",
+    id: 5,
+  },
 ];
 export default function Navbar() {
-	const menuRef = useRef<HTMLDivElement>(null);
-	const [open, setopen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const [open, setopen] = useState(false);
 
-	const toggleMenu = () => {
-		menuRef.current?.classList.toggle("on");
-		setopen(!open);
-	};
-	return (
-		<div className="sticky left-0 top-0 z-50 h-[68px] w-full bg-[rgba(106,106,106,0.52)] text-white backdrop-blur-sm lg:h-[100px]">
-			<div className="mx-auto hidden w-full items-center justify-center gap-5 lg:flex xl:gap-[129px]">
-				<div className="w-fit">
-					<img
-						src="/upexworld.png"
-						alt="upexworld logo"
-						className="h-[58px] w-[265px]"
-					/>
-				</div>
-				<div className="plus-jakarta-sans-600 hidden items-center justify-center gap-12 py-[39px] lg:flex">
-					{menuItems.map((menu) => {
-						return (
-							<div
-								key={menu.label + menu.id}
-								className="group relative cursor-pointer"
-							>
-								<p className="text-[18px] capitalize leading-[22.68px]">
-									<a href="#logoBox">{menu.label}</a>
-								</p>
-								<span className="absolute -bottom-2 left-0 h-1 w-0 bg-blue-300 transition-all duration-500 ease-in-out group-hover:w-full" />
-							</div>
-						);
-					})}
-				</div>
-				<div className="hidden lg:block">
-					<button
-						type="button"
-						className="plus-jakarta-sans-600 h-[45px] w-[158px] rounded-[10px] bg-bg-color p-[10px_20px_10px_20px] leading-[25.2px]"
-					>
-						Get a Quota
-					</button>
-				</div>
-			</div>
-			{/* mobile menu */}
-			<div className="relative flex h-full w-full items-center justify-between px-[2rem] lg:hidden">
-				<div className="w-fit">
-					<img
-						src="/upexworld.png"
-						alt="upexworld logo"
-						className="h-[3rem] w-[100%]"
-					/>
-				</div>
-				<div className="size-[44px] cursor-pointer rounded-sm bg-bg-color">
-					{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
-					<div ref={menuRef} id="toggle" onClick={toggleMenu}>
-						<div className="one" />
-						<div className="two" />
-						<div className="three" />
-					</div>
-				</div>
-				<div
-					className={`absolute right-0 top-[4rem] z-[9999] h-[150dvh] bg-blue-900 transition-all duration-500 ease-in-out ${open ? "fadeIn w-[300px] md:w-[500px]" : "fadeOut w-0"}`}
-				>
-					{menuItems.map((m) => {
-						return (
-							<div
-								key={m.id}
-								className="flex flex-col items-start pl-[3rem] pt-[3rem]"
-							>
-								<p
-									key={m.id}
-									className={
-										"text-lg capitalize transition-all duration-500 ease-in-out md:text-[4rem]"
-									}
-								>
-									{m.label}
-								</p>
-							</div>
-						);
-					})}
-				</div>
-			</div>
-		</div>
-	);
+  const toggleMenu = () => {
+    menuRef.current?.classList.toggle("on");
+    setopen(!open);
+  };
+  return (
+    <div className="sticky left-0 top-0 z-50 h-[68px] w-full bg-[rgba(106,106,106,0.52)] text-white backdrop-blur-sm lg:h-[100px]">
+      <div className="mx-auto hidden w-full items-center justify-center gap-5 lg:flex xl:gap-[129px]">
+        <div className="w-fit">
+          <img
+            src="/upexworld.png"
+            alt="upexworld logo"
+            className="h-[58px] w-[265px]"
+          />
+        </div>
+        <div className="plus-jakarta-sans-600 hidden items-center justify-center gap-12 py-[39px] lg:flex">
+          {menuItems.map((menu) => {
+            return (
+              <div
+                key={menu.label + menu.id}
+                className="group relative cursor-pointer"
+              >
+                <p className="text-[18px] capitalize leading-[22.68px]">
+                  <a href="#logoBox">{menu.label}</a>
+                </p>
+                <span className="absolute -bottom-2 left-0 h-1 w-0 bg-blue-300 transition-all duration-500 ease-in-out group-hover:w-full" />
+              </div>
+            );
+          })}
+        </div>
+        <div className="hidden lg:block">
+          <button
+            type="button"
+            className="plus-jakarta-sans-600 h-[45px] w-[158px] rounded-[10px] bg-bg-color p-[10px_20px_10px_20px] leading-[25.2px]"
+          >
+            Get a Quota
+          </button>
+        </div>
+      </div>
+      {/* mobile menu */}
+      <div className="relative flex h-full w-full items-center justify-between px-[2rem] lg:hidden">
+        <div className="w-fit">
+          <img
+            src="/upexworld.png"
+            alt="upexworld logo"
+            className="h-[3rem] w-[100%]"
+          />
+        </div>
+        <div className="size-[44px] cursor-pointer rounded-sm bg-bg-color">
+          {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+          <div ref={menuRef} id="toggle" onClick={toggleMenu}>
+            <div className="one" />
+            <div className="two" />
+            <div className="three" />
+          </div>
+        </div>
+        <div
+          className={`absolute right-0 top-[4rem] z-[9999] h-[150dvh] bg-blue-900 transition-all duration-500 ease-in-out ${open ? "fadeIn w-[300px] md:w-[500px]" : "fadeOut w-0"}`}
+        >
+          {menuItems.map((m) => {
+            return (
+              <div
+                key={m.id}
+                className="flex flex-col items-start pl-[3rem] pt-[3rem]"
+              >
+                <p
+                  key={m.id}
+                  className={
+                    "text-lg capitalize transition-all duration-500 ease-in-out md:text-[4rem]"
+                  }
+                >
+                  {m.label}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
