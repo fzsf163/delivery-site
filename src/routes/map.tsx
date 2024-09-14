6ef00cd3708876db9d7a222f865d733558e7a652
fzsf163 <yofaisol619@gmail.com>
2024-09-15 03:34:20 +0600

    responsive

--- src/routes/map.tsx
+++ src/routes/map.tsx
@@ -2,7 +2,7 @@ import map from "../assets/contact/map/image 1.webp";
 
 function MapOfLocation() {
   return (
-    <div className="mx-auto w-fit">
+    <div className="mx-auto w-fit p-4 xl:p-0">
       <img src={map} alt="" className="rounded" />
     </div>
   );
