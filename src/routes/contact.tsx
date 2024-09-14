2554864d90d5e3995c9c58a9b8873939a4c7cc63
fzsf163 <yofaisol619@gmail.com>
2024-09-15 03:33:03 +0600

    map added

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,11 +1,13 @@
 import BoxForContact from "../components/contact/contactBoxes";
 import ContactHero from "../components/contact/contactHero";
+import MapOfLocation from "./map";
 
 function Contact() {
   return (
     <div className="space-y-10">
       <ContactHero></ContactHero>
       <BoxForContact></BoxForContact>
+      <MapOfLocation></MapOfLocation>
     </div>
   );
 }
---
+++ src/routes/map.tsx
@@ -0,0 +1,11 @@
+import map from "../assets/contact/map/image 1.webp";
+
+function MapOfLocation() {
+  return (
+    <div className="mx-auto w-fit">
+      <img src={map} alt="" className="rounded" />
+    </div>
+  );
+}
+
+export default MapOfLocation;
