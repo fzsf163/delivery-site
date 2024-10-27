ae360e220c1d1aa9e4f1ab6d1af6ca9d935c0156
fzsf163 <yofaisol619@gmail.com>
2024-10-27 23:21:32 +0600

    txt color chng

--- src/components/contact/contactHero.tsx
+++ src/components/contact/contactHero.tsx
@@ -9,7 +9,7 @@ function ContactHero() {
         className="col-span-full col-start-1 row-start-1 h-full w-full"
       />
       <div className="flex h-full w-full items-center justify-center bg-black/30 lg:justify-start lg:pl-[20rem] col-span-full col-start-1 row-start-1">
-        <p className="plus-jakarta-sans-700 w-fit text-4xl sm:text-6xl text-white">
+        <p className="plus-jakarta-sans-700 w-fit text-4xl sm:text-6xl text-red-600">
           Contact
         </p>
       </div>
