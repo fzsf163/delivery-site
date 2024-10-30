7073eaf1ca0eb70ed9731f182f51f183d2d743a3
fzsf163 <yofaisol619@gmail.com>
2024-10-30 20:58:36 +0600

    height changed, bg-black , h-45rem

--- src/components/contact/contactHero.tsx
+++ src/components/contact/contactHero.tsx
@@ -2,14 +2,14 @@ import img from "../../assets/contact/aerial-view-container-cargo-ship-sea 2.web
 
 function ContactHero() {
   return (
-    <div className="grid h-[20rem] w-full sm:h-full">
-      <img
-        src={img}
-        alt=""
-        className="col-span-full col-start-1 row-start-1 h-full w-full"
-      />
-      <div className="flex h-full w-full items-center justify-center bg-black/30 lg:justify-start lg:pl-[20rem] col-span-full col-start-1 row-start-1">
-        <p className="plus-jakarta-sans-700 w-fit text-4xl sm:text-6xl text-red-600">
+    <div className="w-full sm:h-full">
+      {/*<img*/}
+      {/*  src={img}*/}
+      {/*  alt=""*/}
+      {/*  className="col-span-full col-start-1 row-start-1 h-full w-full"*/}
+      {/*/>*/}
+      <div className="flex w-full items-center justify-center bg-black h-[20rem] sm:h-[45rem] lg:justify-start lg:pl-[20rem]">
+        <p className="plus-jakarta-sans-700 w-fit text-4xl text-red-600 sm:text-6xl">
           Contact
         </p>
       </div>
