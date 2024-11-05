cbdcd4c85b7a9f7398ade65d4603febfd9d0d90d
fzsf163 <yofaisol619@gmail.com>
2024-11-05 18:36:44 +0600

    updated services section

--- src/components/services/services.tsx
+++ src/components/services/services.tsx
@@ -1,14 +1,12 @@
-import img3 from "../../assets/jumbo-jet-flying-sky1.png";
 import m2 from "../../assets/Mask-group2.png";
-import img1 from "../../assets/transport-logistics-concept(1)1.png";
-import img2 from "../../assets/transport-logistics-concept1.png";
-
+import services from "./data.json";
+import ServiceBlock from "./showblocks";
 export default function Services() {
   return (
     <div
       style={{
         backgroundImage: `url("${m2}")`,
-        backgroundSize: "74%",
+        backgroundSize: "100%",
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center bottom",
         objectFit: "cover",
@@ -25,86 +23,18 @@ export default function Services() {
       </div>
 
       <div className="mt-[20px] flex flex-wrap items-center justify-center gap-[30px] pb-10">
-        <div
-          style={{
-            backgroundImage: `url("${img1}")`,
-            backgroundSize: "100%",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center center",
-            objectFit: "cover",
-          }}
-          className="m-2 h-[513px] w-[420px] rounded-[20px]"
-        >
-          <div className="relative h-full rounded-[20px] bg-gradient-to-t from-red-500/40 to-red-600/40">
-            <div className="absolute bottom-0 left-0 flex flex-col items-start gap-5 p-[40px_20px] text-white">
-              <p className="plus-jakarta-sans-700 h-[47px] w-[109px] rounded-[32px] bg-red-600 pt-[9px] text-center text-[24px] leading-[30.24px]">
-                Road
-              </p>
-              <p className="plus-jakarta-sans-700 text-[24px] leading-[30.24px]">
-                Land Freight Transportation
-              </p>
-              <p className="plus-jakarta-sans-400 text-[18px] leading-[25.2px]">
-                For deliveries that don’t need to go by air, we also offer land
-                transport. We use trucks to safely get your packages where they
-                need to go, whether it’s nearby or far away.
-              </p>
-            </div>
-          </div>
-        </div>
+        {services.map((s) => {
+          return (
+            <ServiceBlock
+              bgImage={s.bgImage}
+              name={s.name}
+              sub={s.sub}
+              body={s.body}
+            ></ServiceBlock>
+          );
+        })}
 
-        <div
-          style={{
-            backgroundImage: `url("${img3}")`,
-            backgroundSize: "100%",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center center",
-            objectFit: "cover",
-          }}
-          className="m-2 h-[513px] w-[420px] rounded-[20px]"
-        >
-          <div className="relative h-full rounded-[20px] bg-gradient-to-t from-red-500/40 to-red-600/40">
-            <div className="absolute bottom-0 left-0 flex flex-col items-start gap-5 p-[40px_20px] text-white">
-              <p className="plus-jakarta-sans-700 h-[47px] w-[109px] rounded-[32px] bg-red-600 pt-[9px] text-center text-[24px] leading-[30.24px]">
-                Air
-              </p>
-              <p className="plus-jakarta-sans-700 text-[24px] leading-[30.24px]">
-                Air Freight Transportation
-              </p>
-              <p className="plus-jakarta-sans-400 text-[18px] leading-[25.2px]">
-                If you need to send something quickly, our air freight service
-                is perfect! We’ll help you get your packages on a plane, so they
-                can reach their destination fast.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div
-          style={{
-            backgroundImage: `url("${img2}")`,
-            backgroundSize: "100%",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center center",
-            objectFit: "cover",
-          }}
-          className="m-2 h-[513px] w-[420px] rounded-[20px]"
-        >
-          <div className="relative h-full rounded-[20px] bg-gradient-to-t from-red-500/40 to-red-600/40">
-            <div className="absolute bottom-0 left-0 flex flex-col items-start gap-5 p-[40px_20px] text-white">
-              <p className="plus-jakarta-sans-700 h-[47px] w-[109px] rounded-[32px] bg-red-600 pt-[9px] text-center text-[24px] leading-[30.24px]">
-                Water
-              </p>
-              <p className="plus-jakarta-sans-700 text-[24px] leading-[30.24px]">
-                Ship Freight Transportation
-              </p>
-              <p className="plus-jakarta-sans-400 text-[18px] leading-[25.2px]">
-                Get your goods delivered quickly and easily! We help move
-                packages from one place to another, even across countries, and
-                you can track everything step by step.
-              </p>
-            </div>
-          </div>
-        </div>
+     
       </div>
     </div>
   );
---
+++ src/components/services/showblocks.tsx
@@ -0,0 +1,35 @@
+interface ServiceBlockPrps {
+  bgImage: string;
+  name: string;
+  sub: string;
+  body: string;
+}
+
+function ServiceBlock({ bgImage, body, name, sub }: ServiceBlockPrps) {
+  return (
+    <div
+      style={{
+        backgroundImage: `url("${bgImage}")`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+      }}
+      className="relative h-[513px] w-[420px] rounded-[20px] sm:m-2"
+    >
+      <div className="absolute inset-0 rounded-[20px] bg-gradient-to-t from-red-500/40 to-red-600/40"></div>
+      <div className="absolute bottom-0 left-0 flex flex-col items-start gap-5 rounded-[20px] p-[40px_20px] text-white">
+        <p className="plus-jakarta-sans-700 h-[47px] w-[109px] rounded-[32px] bg-red-600 pt-[9px] text-center text-[24px] uppercase leading-[30.24px]">
+          {name}
+        </p>
+        <p className="plus-jakarta-sans-700 text-[24px] capitalize leading-[30.24px]">
+          {sub}
+        </p>
+        <p className="plus-jakarta-sans-400 text-[18px] leading-[25.2px]">
+          {body}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ServiceBlock;
